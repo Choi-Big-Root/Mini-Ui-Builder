26bf76bab5868f9ef20eb3a21cfabb7cb5a49603
Choi-Big-Root <bigroot0601@gmail.com>
2025-07-08 12:32:58 +0900

    [20250708] BlocBuilder를 통한 위젯 선택 상태별 UI 처리 추가
    - Bloc 상태에 따라 로딩 인디케이터, 에러 메시지, 데이터 렌더링 분기 처리
    - WidgetSelectorLoading → CircularProgressIndicator 표시
    - WidgetSelectorError → 에러 메시지 출력
    - WidgetSelectorLoaded → 위젯 목록 UI 렌더링 준비
    
    * ClientException: Failed to fetch 에러 발생 중 확인필요. 실제 근무 상황일경우 에러를 잡고 git에 올려야 한다.

--- lib/presentation/main/widget/widget_selector_panel.dart
+++ lib/presentation/main/widget/widget_selector_panel.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mini_ui_builder/presentation/main/bloc/widget_selector_bloc.dart';
+import 'package:mini_ui_builder/presentation/main/bloc/widget_selector_state.dart';
 
 class WidgetSelectorPanel extends StatelessWidget {
   const WidgetSelectorPanel({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    // BlocBuilder : Bloc 상태가 변경될때 마다 builder 함수가 호출 되어 ui fmf ektl rmflsek.
+    return BlocBuilder<WidgetSelectorBloc, WidgetSelectorState>(
+      builder: (context, state) {
+        // 상태가 로딩 중일때
+        if (state is WidgetSelectorLoading) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        // 상태가 에러일 경우
+        if (state is WidgetSelectorError) {
+          print(state.message);
+          return Center(child: Text('에러: ${state.message}'));
+        }
+
+        // 상태가 데이터 로드 완료일 경우
+        if (state is WidgetSelectorLoaded) {
+          final widgetList = state.widgetList;
+          return GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+            ),
+            itemCount: widgetList.length,
+            itemBuilder: (context, index) {
+              final item = widgetList[index];
+              return Card(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Text(item.icon), Text(item.name)],
+                ),
+              );
+            },
+          );
+        }
+        return const SizedBox.shrink();
+      },
+    );
   }
 }
