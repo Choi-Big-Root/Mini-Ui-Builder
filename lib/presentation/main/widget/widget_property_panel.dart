64478f28a0906f8aca8ee5573d53eedd8b4d5209
Choi-Big-Root <bigroot0601@gmail.com>
2025-07-08 16:57:26 +0900

    [20250708] 우측 설정화면 일부 로직만 작성
    왼쪽 Selector Panel에서 위젯을 선택 SelectedWidgetBloc의 상태 변화를 감지하여 선택된 위젯의 정보를 보여주는 화면
    
    Bloc은 사용자의 선택 이벤트를 받아 현재 선택된 위젯 상태를 관리하고 UI는 이 상태 변화를 실시간으로 반영하여 적절한 정보를 표시할 예정!!
    현재는 임시로 editor_view 일부를 작성을 위해
    선택된 위젯 이름이 'Text'일 경우 해당 이름을 출력하는 기본 동작만 구현 해 놓았으며,
    이후에는 사용자가 직접 선택한 위젯의 속성을 수정할 수 있도록 설정 화면으로 확장할 예정입니다.

--- lib/presentation/main/widget/widget_property_panel.dart
+++ lib/presentation/main/widget/widget_property_panel.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class WidgetPropertyPanel extends StatelessWidget {
+import '../bloc/selected_widget/selected_widget_bloc.dart';
+import '../bloc/selected_widget/selected_widget_state.dart';
+
+class WidgetPropertyPanel extends StatefulWidget {
   const WidgetPropertyPanel({super.key});
 
   @override
+  State<WidgetPropertyPanel> createState() => _WidgetPropertyPanelState();
+}
+
+class _WidgetPropertyPanelState extends State<WidgetPropertyPanel> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BlocBuilder<SelectedWidgetBloc, SelectedWidgetState>(
+      builder: (context, state) {
+        if (state is WidgetSelected) {
+          final widget = state.widget;
+          // widget.name에 따라 폼 분기 우선 대기 일단 editor view 단 먼저 해결.
+          if (widget.name == 'Text') {
+            return Center(child: Text(widget.name));
+          }
+        }
+        return const Center(child: Text('위젯을 선택하세요'));
+      },
+    );
   }
 }
