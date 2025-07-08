0672ead380066e2ed571290460231f4855b3a4ff
Choi-Big-Root <bigroot0601@gmail.com>
2025-07-08 15:30:08 +0900

    [20250708] 왼쪽 widget 선택 영역 완료
    - 우측 셋팅 화면 작성 이후 수정이 필요 할 것으로 예상.

--- lib/presentation/main/widget/widget_selector_panel.dart
+++ lib/presentation/main/widget/widget_selector_panel.dart
@@ -3,8 +3,15 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mini_ui_builder/presentation/main/bloc/widget_selector_bloc.dart';
 import 'package:mini_ui_builder/presentation/main/bloc/widget_selector_state.dart';
 
-class WidgetSelectorPanel extends StatelessWidget {
+class WidgetSelectorPanel extends StatefulWidget {
   const WidgetSelectorPanel({super.key});
+
+  @override
+  State<WidgetSelectorPanel> createState() => _WidgetSelectorPanelState();
+}
+
+class _WidgetSelectorPanelState extends State<WidgetSelectorPanel> {
+  int? selectedIndex; // 선택된 카드의 인덱스를 저장하기 위한 변수 : default는 아무런 값을 선택하고 있지않을 테니 null 일 수도 있다.
 
   @override
   Widget build(BuildContext context) {
@@ -24,20 +31,46 @@ class WidgetSelectorPanel extends StatelessWidget {
         // 상태가 데이터 로드 완료일 경우
         if (state is WidgetSelectorLoaded) {
           final widgetList = state.widgetList;
-          return GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-            ),
-            itemCount: widgetList.length,
-            itemBuilder: (context, index) {
-              final item = widgetList[index];
-              return Card(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [Text(item.icon), Text(item.name)],
+          return Container(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            color: const Color(0xFF121212),
+            child: Center(
+              // 전체 GrideView 중앙 배치
+              child: GridView.builder(
+                shrinkWrap: true,
+                // 자식 크기만큼의 높이를 가지도록 설정 해당 설정이 없으면 전체크기를 다 잡기에 Center가 의미 없다.
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 2 / 3,
                 ),
-              );
-            },
+                itemCount: widgetList.length,
+                itemBuilder: (context, index) {
+                  final item = widgetList[index];
+                  final isSelected = index == selectedIndex;
+                  return GestureDetector(
+                    onTap: () {
+                      print('일단 대기 아이템 클릭 ${item.name}');
+                      setState(() {
+                        selectedIndex = index;
+                      });
+                    },
+                    child: Card(
+                      color: isSelected? const Color(0xFF633BEB) : const Color(0xFF222222),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(item.icon), // 추후 처리.
+                          Text(
+                            item.name,
+                            style: const TextStyle(color: Color(0xFFC1C1C1)),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
           );
         }
         return const SizedBox.shrink();
