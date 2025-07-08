58b5c76d787deff8b80e3160e7049761d9a876d0
Choi-Big-Root <bigroot0601@gmail.com>
2025-07-08 15:06:46 +0900

    [20250708] ClientException: Failed to fetch 에러 해결
    클라이언트(Flutter Web)에서 API 서버(`localhost:8080`)로 요청 시 CORS(Cross-Origin Resource Sharing) 에러가 발생.
    브라우저의 보안 정책으로 인해, 서로 다른 Origin 간의 요청은 서버가 명시적으로 허용하지 않으면 차단되는것을 확인.
    `shelf_cors_headers` 패키지를 사용해 서버에 미들웨어 CORS 허용 헤더를 추가했습니다.

--- server/bin/main.dart
+++ server/bin/main.dart
@@ -1,6 +1,7 @@
 import 'package:mini_ui_builder_server/handler/selector_widget_handler.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as io;
+import 'package:shelf_cors_headers/shelf_cors_headers.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 // Shelf 서버를 실행하는 진입점 비동기 실행을 위해 async 사용
@@ -13,9 +14,13 @@ void main() async {
   router.get('/selector_widget_list', selectorWidgetHandler);
 
   // 중간단계 미들웨어로 요청 로그 출력 후 실제 요청 처리 함수 실행 = router.call
-  final handler = const Pipeline().addMiddleware(logRequests()).addHandler(router.call);
+  final handler = const Pipeline()
+      .addMiddleware(corsHeaders())
+      .addMiddleware(logRequests())
+      .addHandler(router.call);
 
   // shelf 서버를 localhost:8080 포트에 바인딩 하여 시작. 비동기 임으로 await 사용.
   final server = await io.serve(handler, 'localhost', 8080);
-  print('서버 실행중: http://${server.address.host}:${server.port}'); // print 는 꼭 추후에 삭제 해야한다. 메모리에 계속 떠 있기 때문.
+  print(
+      '서버 실행중: http://${server.address.host}:${server.port}'); // print 는 꼭 추후에 삭제 해야한다. 메모리에 계속 떠 있기 때문.
 }
--- server/lib/data/selector_widget_data.dart
+++ server/lib/data/selector_widget_data.dart
@@ -1,6 +1,11 @@
 final selectorWidgetList = [
-  {"name": "Container", "icon": "container_icon.png"},
-  {"name": "Text", "icon": "text_icon.png"},
-  {"name": "Row", "icon": "row_icon.png"},
-  {"name": "Column", "icon": "column_icon.png"},
+  {"name": "Row", "icon": "image1"},
+  {"name": "Column", "icon": "image2"},
+  {"name": "Stack", "icon": "image3"},
+  {"name": "Wrap", "icon": "image4"},
+  {"name": "Grid", "icon": "image5"},
+  {"name": "List", "icon": "image6"},
+  {"name": "Card", "icon": "image7"},
+  {"name": "Spacer", "icon": "image8"},
+  {"name": "Text", "icon": "image9"},
 ];
