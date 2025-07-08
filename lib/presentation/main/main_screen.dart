946a08a62b612e60c4986a4c50d0c731b72cc23f
Choi-Big-Root <bigroot0601@gmail.com>
2025-07-08 12:15:34 +0900

    [20250708] WidgetSelectorBloc 주입 및 초기 이벤트 실행 로직 추가
    - BlocProvider를 통해 WidgetSelectorBloc을 위젯 트리에 주입
    - RemoteDataSource → Repository → UseCase → Bloc 계층 구조 구성
    - Bloc 생성 시 FetchWidgetSelectorList 이벤트 자동 실행하여 초기 데이터 로딩

--- lib/presentation/main/main_screen.dart
+++ lib/presentation/main/main_screen.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mini_ui_builder/data/repository/widget_selector_repository_impl.dart';
+import 'package:mini_ui_builder/data/source/widget_selector_remote_data_source.dart';
+import 'package:mini_ui_builder/domain/usecase/fetch_selector_widget_list_usecase.dart';
+import 'package:mini_ui_builder/presentation/main/bloc/widget_selector_bloc.dart';
+import 'package:mini_ui_builder/presentation/main/bloc/widget_selector_event.dart';
 import 'package:mini_ui_builder/presentation/main/widget/widget_editor_view.dart';
 import 'package:mini_ui_builder/presentation/main/widget/widget_property_panel.dart';
 import 'package:mini_ui_builder/presentation/main/widget/widget_selector_panel.dart';
@@ -18,12 +24,24 @@ class _MainScreenState extends State<MainScreen> {
     return Row(
       children: [
         isLeftPanelOpen
-            ? const Expanded(
+            ? Expanded(
                 flex: 2,
-                child: WidgetSelectorPanel(),
+                child: BlocProvider(
+                  // 1 : Bloc 생성 + UseCase에 RepositoryImpl 주입
+                  create: (context) => WidgetSelectorBloc(
+                    FetchSelectorWidgetListUseCase(
+                      // 2: 실제 데이터를 가져올 데이터 소스 구현체 주입
+                      WidgetSelectorRepositoryImpl(
+                        WidgetSelectorRemoteDataSource(),
+                      ),
+                    ),
+                    // 3: 생성 직후 '위젯 리스트 가져오기' 이벤트를 디스패치
+                  )..add(FetchWidgetSelectorList()),
+                  // 4 : 해당 Bloc를 소비할 하위 위젯
+                  child: const WidgetSelectorPanel(),
+                ),
               )
             : const SizedBox.shrink(), // null 대신 사용 위젯.
-
         // 왼쪽 Widget Selecotor 판넬을 표시,제거 할 수 있도록.
         GestureDetector(
           onTap: () {
@@ -35,19 +53,11 @@ class _MainScreenState extends State<MainScreen> {
             color: Colors.grey[200],
             width: 24,
             alignment: Alignment.center,
-            child: Icon(
-              isLeftPanelOpen ? Icons.arrow_left : Icons.arrow_right,
-            ),
+            child: Icon(isLeftPanelOpen ? Icons.arrow_left : Icons.arrow_right),
           ),
         ),
-        const Expanded(
-          flex: 5,
-          child: WidgetEditorView(),
-        ),
-        const Expanded(
-          flex: 3,
-          child: WidgetPropertyPanel(),
-        ),
+        const Expanded(flex: 5, child: WidgetEditorView()),
+        const Expanded(flex: 3, child: WidgetPropertyPanel()),
       ],
     );
   }
