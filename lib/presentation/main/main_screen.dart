9387b5ce5f777527797a0ab9e1d806679a8a254b
Choi-Big-Root <bigroot0601@gmail.com>
2025-07-08 16:18:19 +0900

    [20250708]BlocProvider에서 MultiBlocProvider로 구조 변경
    - WidgetSelectorBloc, SelectedWidgetBloc 을 함께 관리하기 위해 MultiBlocProvider 적용
    - 여러 Bloc을 사용하는 구조에 대비한 확장성과 가독성 향상
    
    *BlocProvider를 Row 내부에 작성했던 구조는 동작은 하지만
    Bloc과 UI의 분리가 명확하지 않아 유지보수 측면에서 선호되지 않는 방식임이라는 것을 알게 되었습니다.
    SelectedWidgetBloc 를 추가하면서 MultiBlocProvider로 개선하여 Bloc을 UI 외부에서 관리하도록 구조 변경 했습니다.

--- lib/presentation/main/main_screen.dart
+++ lib/presentation/main/main_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mini_ui_builder/data/repository/widget_selector_repository_impl.dart';
 import 'package:mini_ui_builder/data/source/widget_selector_remote_data_source.dart';
 import 'package:mini_ui_builder/domain/usecase/fetch_selector_widget_list_usecase.dart';
+import 'package:mini_ui_builder/presentation/main/bloc/selected_widget/selected_widget_bloc.dart';
 import 'package:mini_ui_builder/presentation/main/bloc/widget_selector/widget_selector_bloc.dart';
 import 'package:mini_ui_builder/presentation/main/bloc/widget_selector/widget_selector_event.dart';
 import 'package:mini_ui_builder/presentation/main/widget/widget_editor_view.dart';
@@ -21,44 +22,45 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        isLeftPanelOpen
-            ? Expanded(
-                flex: 2,
-                child: BlocProvider(
-                  // 1 : Bloc 생성 + UseCase에 RepositoryImpl 주입
-                  create: (context) => WidgetSelectorBloc(
-                    FetchSelectorWidgetListUseCase(
-                      // 2: 실제 데이터를 가져올 데이터 소스 구현체 주입
-                      WidgetSelectorRepositoryImpl(
-                        WidgetSelectorRemoteDataSource(),
-                      ),
-                    ),
-                    // 3: 생성 직후 '위젯 리스트 가져오기' 이벤트를 디스패치
-                  )..add(FetchWidgetSelectorList()),
-                  // 4 : 해당 Bloc를 소비할 하위 위젯
-                  child: const WidgetSelectorPanel(),
-                ),
-              )
-            : const SizedBox.shrink(), // null 대신 사용 위젯.
-        // 왼쪽 Widget Selecotor 판넬을 표시,제거 할 수 있도록.
-        GestureDetector(
-          onTap: () {
-            setState(() {
-              isLeftPanelOpen = !isLeftPanelOpen;
-            });
-          },
-          child: Container(
-            color: Colors.grey[200],
-            width: 24,
-            alignment: Alignment.center,
-            child: Icon(isLeftPanelOpen ? Icons.arrow_left : Icons.arrow_right),
-          ),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => WidgetSelectorBloc(
+            FetchSelectorWidgetListUseCase(
+              // 2: 실제 데이터를 가져올 데이터 소스 구현체 주입
+              WidgetSelectorRepositoryImpl(WidgetSelectorRemoteDataSource()),
+            ),
+            // 3: 생성 직후 '위젯 리스트 가져오기' 이벤트를 디스패치
+          )..add(FetchWidgetSelectorList()),
         ),
-        const Expanded(flex: 5, child: WidgetEditorView()),
-        const Expanded(flex: 3, child: WidgetPropertyPanel()),
+
+        BlocProvider(create: (context) => SelectedWidgetBloc()),
       ],
+      child: Row(
+        children: [
+          isLeftPanelOpen
+              ? const Expanded(flex: 2, child: WidgetSelectorPanel())
+              : const SizedBox.shrink(), // null 대신 사용 위젯.
+          // 왼쪽 Widget Selecotor 판넬을 표시,제거 할 수 있도록.
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                isLeftPanelOpen = !isLeftPanelOpen;
+              });
+            },
+            child: Container(
+              color: Colors.grey[200],
+              width: 24,
+              alignment: Alignment.center,
+              child: Icon(
+                isLeftPanelOpen ? Icons.arrow_left : Icons.arrow_right,
+              ),
+            ),
+          ),
+          const Expanded(flex: 5, child: WidgetEditorView()),
+          const Expanded(flex: 3, child: WidgetPropertyPanel()),
+        ],
+      ),
     );
   }
 }
